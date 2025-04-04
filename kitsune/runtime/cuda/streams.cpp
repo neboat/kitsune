3c4dd4c6571a3a14b255e1934b391bc25fba1b4d
TB Schardl <neboat@mit.edu>
2025-04-03 22:31:26 -0400

    [kitrt] Add more verbose output for CUDA stream management.

--- kitsune/runtime/cuda/streams.cpp
+++ kitsune/runtime/cuda/streams.cpp
@@ -164,6 +164,8 @@ void __kitcuda_sync_thread_stream(void *opaque_stream) {
   assert(opaque_stream != nullptr && "unexpected null stream pointer!");
   KIT_NVTX_PUSH("kitcuda:sync_thread_stream", KIT_NVTX_STREAM);
   CUstream stream = (CUstream)opaque_stream;
+  if (__kitrt_verbose_mode())
+    fprintf(stderr, "kitcuda: synchronizing stream: %p.\n", (void *)stream);
   CU_SAFE_CALL(cuStreamSynchronize_p(stream));
   KIT_NVTX_POP();
 }
