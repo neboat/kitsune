736bc47a31faa3996e0e8caa5059ec4d7f3311dd
TB Schardl <neboat@mit.edu>
2025-04-08 07:13:57 -0400

    [CudaABI] Fix handling of CUDA streams, and work around issue with control flow that can inadvertently call kitrt to sync the NULL stream when it's not used.

--- kitsune/runtime/cuda/streams.cpp
+++ kitsune/runtime/cuda/streams.cpp
@@ -52,6 +52,7 @@
 #include "kitcuda_dylib.h"
 #include <algorithm>
 #include <deque>
+#include <cstddef>
 #include <mutex>
 #include <optional>
 #include <stdio.h>
@@ -162,6 +163,10 @@ void *__kitcuda_get_thread_stream() {
 
 void __kitcuda_sync_thread_stream(void *opaque_stream) {
   assert(opaque_stream != nullptr && "unexpected null stream pointer!");
+  if (opaque_stream == nullptr)
+    // TODO: Allow syncs of NULL stream?  Need to avoid adding the NULL stream
+    // to _kitcuda_streams in that case.
+    return;
   KIT_NVTX_PUSH("kitcuda:sync_thread_stream", KIT_NVTX_STREAM);
   CUstream stream = (CUstream)opaque_stream;
   if (__kitrt_verbose_mode())
